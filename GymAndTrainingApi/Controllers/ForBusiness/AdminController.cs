c1be79c4ef9619346540d067eb32fdbd733220ed
dimas <you@example.comdimon3456lolkek@gmail.com>
2026-06-30 01:59:34 +0300

    Put endpoint get-all-members-with-last-subscription to AdminController

---
+++ GymAndTrainingApi/Controllers/ForBusiness/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Gym.Application.Members.Dto_s;
+using Gym.Application.Members.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.Api.Controllers.ForBusiness
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AdminController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("members/get-all-members-with-last-subscription")]
+        public async Task<IEnumerable<MemberReadDto>> GetAllMembers()
+        {
+            return await _mediator.Send(new GetAllMembersWithLatestSubscriptionQuery());
+        }
+    }
+}
