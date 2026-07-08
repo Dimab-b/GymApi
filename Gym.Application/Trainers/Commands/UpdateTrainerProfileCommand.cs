83c66f1479a864581e9d05a8d067f487cecbe8c4
dimas <you@example.comdimon3456lolkek@gmail.com>
2026-07-09 01:23:11 +0300

    Added updating profile for trainers

---
+++ Gym.Application/Trainers/Commands/UpdateTrainerProfileCommand.cs
@@ -0,0 +1,37 @@
+﻿using Gym.Domain.Common;
+using Gym.Domain.Common.VO;
+using Gym.Domain.Trainers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Application.Trainers.Commands
+{
+    public record UpdateTrainerProfileCommand(Guid Id , string Name, string Specialization) : IRequest;
+
+
+    public class UpdateTrainerProfileCommandHandler : IRequestHandler<UpdateTrainerProfileCommand>
+    {
+        private readonly ITrainerRepository _trainerRepository;
+        private readonly IUnitOfWork _uow;
+
+        public UpdateTrainerProfileCommandHandler(ITrainerRepository trainerRepository , IUnitOfWork uow)
+        {
+            _trainerRepository = trainerRepository;
+            _uow = uow;
+        }
+
+        public async Task Handle(UpdateTrainerProfileCommand command , CancellationToken cancellationToken = default)
+        {
+            var trainer = await _trainerRepository.GetByIdAsync(command.Id, cancellationToken) ?? throw new KeyNotFoundException("Trainer not found");
+
+            trainer.UpdateProfile(command.Name , command.Specialization);
+
+            await _uow.SaveChangesAsync(cancellationToken);
+        }
+    }
+    
+
+
+}
